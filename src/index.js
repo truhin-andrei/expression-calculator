b5585938461b709a59837ef7989a965d41b8e6e2
truhin-andrei <truhin-andrei@bk.ru>
2019-10-05 15:42:49 +0700

    feat: add solution for task

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ function expressionCalculator(expr) {
     expr = expr.trim();
     expr = expr.split(" ");
     expr.forEach ((item, index, expr) => {
-        if (item == ""){
+    	 if (item == ""){
             expr.splice (index, 1);
         }
         if (item.length > 1 ){
@@ -37,16 +37,44 @@ function expressionCalculator(expr) {
         if (item.includes('/')){
             expr.splice (index, 1, item.split('/')[0], '/', item.split('/')[1]);
         }
-       
     }
     });
     }
-    if ((expr.indexOf('(') != -1) && (expr.lastIndexOf(')') != -1)) {
-       let a ;
-       a = expressionCalculator(expr.slice(expr.indexOf('(')+1, expr.lastIndexOf(')')));
-       expr.splice (expr.indexOf('('), (expr.lastIndexOf(')')-expr.indexOf('(')+1), a);
+    /*проверка на скобочки*/
+
+    while ((expr.indexOf('(') != -1) && (expr.lastIndexOf(')') != -1)) {
+       let a, start, final ;
+       start= expr.indexOf('(');
+       final = expr.indexOf(')');
+       if ((expr.slice(start+1, final).includes('(') !=-1) /*&& (expr.slice(start+1, final).includes(')') ==-1)*/) {
+       	let openCount, closeCount;
+       	openCount=0; 
+       	closeCount = 0;
+       	 expr.slice(start, final+1).forEach((item,index)=>{
+       		if (item == '(') openCount++;
+       		if (item == ')') closeCount++;
+       		});
+       	if (closeCount > openCount) throw "ExpressionError: Brackets must be paired";
+       	if (closeCount < openCount){
+       		
+       		do {
+       			final++;
+       			if (expr[final] == '(') openCount++;
+       			if (expr[final] == ')') closeCount++;
+       			if ((openCount!=closeCount) && (final == expr.length-1)){
+       				throw "ExpressionError: Brackets must be paired";
+       			}
+       	    		}
+       		while (openCount != closeCount);
+       	}
+       
+       }
+       a = expressionCalculator(expr.slice(start+1, final));
+       expr.splice (start, (final-start+1), a);
     }
-    if (((expr.indexOf('(') != -1) && (expr.indexOf(')') == -1)) || ((expr.indexOf('(') == -1) && (expr.indexOf(')') != -1))) {
+
+
+    if (((expr.indexOf('(') != -1) && (expr.lastIndexOf(')') == -1)) || ((expr.indexOf('(') == -1) && (expr.lastIndexOf(')') != -1))) {
         throw "ExpressionError: Brackets must be paired";
     }
     if ((expr.indexOf('(') == -1) && (expr.lastIndexOf(')') == -1)){
@@ -65,12 +93,16 @@ function expressionCalculator(expr) {
             }
         }
 
-        for (let i=0; i<expr.length; i++) {
+         for (let i=0; i<expr.length; i++) {
             if (expr[i] == '-') {
-                expr.splice (i-1, 3, (Number(expr[i-1]) - Number(expr[i+1])));
+                if (Number(expr[i+1] <0)) {
+                     expr.splice (i-1, 3, (Number(expr[i-1]) + (-1* Number(expr[i+1]))));
+                }
+                else expr.splice (i-1, 3, (Number(expr[i-1]) - Number(expr[i+1])));
                 i--;
             }
         }
+
 
         for (let i=0; i<expr.length; i++) {
             if (expr[i] == '+') {
